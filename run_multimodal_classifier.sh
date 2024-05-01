9b6b654c4c28cc7759ad3c0622a673a759a6ea29
“Waterfwater <freshwater_eata@163.com>
2024-05-01 23:05:46 +0800

    1

--- run_multimodal_classifier.sh
+++ run_multimodal_classifier.sh
@@ -9,7 +9,7 @@ do
         do
             echo ${j}
             PYTHONIOENCODING=utf-8 CUDA_VISIBLE_DEVICES=1 python run_multimodal_classifier.py --data_dir \
-            ../../input/twitter15-and-17/depression/depression/${i} --task_name ${i} --output_dir ./output/${i}_${k}_${j}_mm_output/ \
+            ../../input/twitter15-and-17/depression/${i} --task_name ${i} --output_dir ./output/${i}_${k}_${j}_mm_output/ \
             --bert_model bert-base-uncased --do_train --do_eval --train_batch_size 32 --mm_model ${k} --pooling ${j}
         done
     done
